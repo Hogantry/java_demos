f8d30f6923ff327e0d18e533af6524ce3180da51
Mac <dengfazhi@DFZ.local>
2020-09-06 11:35:53 +0800

    lucene

--- lucene/src/main/java/com/dfz/lucene/SearchFiles.java
+++ lucene/src/main/java/com/dfz/lucene/SearchFiles.java
@@ -11,6 +11,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.BufferedReader;
@@ -32,11 +33,16 @@ public class SearchFiles {
     public static void main(String[] args) throws IOException, ParseException {
         String indexPath = "lucene/src/main/resources/index";
         String contentField = "contents";
-        IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+        // 索引目录对象
+        Directory directory = FSDirectory.open(Paths.get(indexPath));
+        // 索引读取工具
+        IndexReader indexReader = DirectoryReader.open(directory);
+        // 索引搜索工具
         IndexSearcher indexSearcher = new IndexSearcher(indexReader);
         Analyzer standardAnalyzer = new StandardAnalyzer();
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        // 创建查询解析器,两个参数：默认要查询的字段的名称，分词器
         QueryParser queryParser = new QueryParser(contentField, standardAnalyzer);
         System.out.println("Enter query:");
         // 从Console读取要查询的语句
@@ -48,6 +54,7 @@ public class SearchFiles {
         if (line.length() == 0) {
             return;
         }
+        // 创建查询对象
         Query query = queryParser.parse(line);
         System.out.println("Searching for:" + query.toString(contentField));
         doPagingSearch(indexSearcher, query);
@@ -56,14 +63,22 @@ public class SearchFiles {
     }
 
     private static void doPagingSearch(IndexSearcher indexSearcher, Query query) throws IOException {
+        // 搜索数据,两个参数：查询条件对象,要查询的最大结果条数N
+        // 返回的结果是 按照匹配度排名得分前N名的文档信息（包含查询到的总条数信息、所有符合条件的文档的编号信息）。
         TopDocs results = indexSearcher.search(query, 10);
-        ScoreDoc[] hits = results.scoreDocs;
+        // 获取得分文档对象（ScoreDoc）数组.ScoreDoc中包含：文档的编号、文档的得分
+        ScoreDoc[] scoreDocs = results.scoreDocs;
+        // 获取总条数
         int numTotalHits = Math.toIntExact(results.totalHits.value);
         System.out.println(numTotalHits + " total matching documents");
-        for (ScoreDoc hit : hits) {
-            Document document = indexSearcher.doc(hit.doc);
+        for (ScoreDoc scoreDoc : scoreDocs) {
+            // 取出文档编号
+            int docID = scoreDoc.doc;
+            // 根据编号去找文档
+            Document document = indexSearcher.doc(docID);
             System.out.println("文档:" + document.get("path"));
-            System.out.println("相关度:" + hit.score);
+            // 取出文档得分
+            System.out.println("相关度:" + scoreDoc.score);
             System.out.println("================================");
         }
     }
